34c9c1754f28f670294e09a478c98de9a8f07c92
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-03-07 00:51:57 +0100

    ✨ New file Text

--- src/home.rs
+++ src/home.rs
@@ -2,17 +2,21 @@ use ratatui::buffer::Buffer;
 use ratatui::layout::Flex;
 use ratatui::prelude::*;
 
-const name: &str = "       ██    ██  ███████  ██       ██       ███████
-       ██    ██  ██       ██       ██      ██     ██
-       ████████  ██████   ██       ██      ██     ██
-       ██    ██  ██       ██       ██      ██     ██
-       ██    ██  ███████  ███████  ███████  ███████
+const name_title: &str = r##"
+      ██    ██  ███████  ██       ██       ███████.
+      ██    ██  ██       ██       ██      ██     ██
+      ████████  ██████   ██       ██      ██     ██
+      ██    ██  ██       ██       ██      ██     ██
+      ██    ██  ███████  ███████  ███████  ███████.
 
-██     ███    ██   ███████   ███████    ██       ████████
-██     ███    ██  ██     ██  ██    ██   ██       ██     ██
-██     ███    ██  ██     ██  ███████    ██       ██      ██
- ██   ██ ██  ██   ██     ██  ██    ██   ██       ██     ██
-   ████   ████     ███████   ██    ██   ███████  ████████";
+██     ███    ██   ███████   ███████    ██       ████████    ██
+██     ███    ██  ██     ██  ██    ██   ██       ██     ██   ██
+██     ███    ██  ██     ██  ███████    ██       ██      ██  ██
+ ██   ██ ██  ██   ██     ██  ██    ██   ██       ██     ██.....
+   ████   ████     ███████   ██    ██   ███████  ████████    ██
+"##;
+
+const new_file: &str = r##"+ New File       SPC n"##;
 
 #[derive(Debug, Default)]
 pub struct Home {
@@ -55,7 +59,23 @@ impl Widget for &Home {
         let title_area = inner_center_area[0];
         let main_area = inner_center_area[1];
 
-        let title = Text::raw(name.clone()).centered();
+        let actions_area = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Percentage(20),
+            ])
+            .split(main_area);
+
+        let title: Text = Text::raw(name_title.replace('.', " "))
+            .centered()
+            .blue();
+
+        let new_file_ui: Text = Text::raw(new_file)
+            .centered()
+            .bold()
+            .blue();
+
+        new_file_ui.render(actions_area[0], buf);
         title.render(title_area, buf);
     }
 }
